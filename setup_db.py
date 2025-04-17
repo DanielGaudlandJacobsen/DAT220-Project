2a6327b93da164c75019c5041ff0e1754c90c89c
Chiwegian <ej.feng@stud.uis.no>
2025-04-17 19:55:53 +0200

    b

---
+++ setup_db.py
@@ -0,0 +1,68 @@
+import sqlite3
+from sqlite3 import Error
+from werkzeug.security import generate_password_hash
+
+database = r"./database.db"
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+    return conn
+
+sql_create_users_table = """
+CREATE TABLE IF NOT EXISTS users (
+    user_id INTEGER PRIMARY KEY,
+    username VARCHAR(15) UNIQUE NOT NULL,
+    password VARCHAR(30) NOT NULL,
+    email VARCHAR(40) UNIQUE NOT NULL,
+    role TEXT NOT NULL CHECK(role IN ('admin', 'user')),
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+"""
+
+sql_create_posts_table = """
+CREATE TABLE IF NOT EXISTS posts (
+    post_id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+"""
+
+sql_create_comments_table = """
+CREATE TABLE IF NOT EXISTS comments (
+    comment_id INTEGER PRIMARY KEY,
+    post_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (post_id) REFERENCES posts (post_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+"""
+
+sql_create_likes_table = """
+CREATE TABLE IF NOT EXISTS likes (
+    like_id INTEGER PRIMARY KEY,
+    post_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES posts (post_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+"""
+
+sql_create_followers_table = """
+CREATE TABLE IF NOT EXISTS followers (
+    follower_id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    follower_user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (follower_user_id) REFERENCES users (user_id)
+);
+"""
